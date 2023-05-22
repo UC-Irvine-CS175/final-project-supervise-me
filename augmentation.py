cb5cfdd290181e4a06777bb9feccc71c370f0be5
Thien Toan Vu <67763741+Thientvu@users.noreply.github.com>
2023-05-22 14:21:29 -0700

    added Data_utils.py, augmentation.py, bps_dataset.py

---
+++ augmentation.py
@@ -0,0 +1,146 @@
+'''
+The purpose of augmentations is to increase the size of the training set
+by applying random (or selected) transformations to the training images.
+
+Create augmentation classes for use with the PyTorch Compose class 
+that takes a list of transformations and applies them in order, which 
+can be chained together simply by defining a __call__ method for each class. 
+'''
+import cv2
+import numpy as np
+import torch
+from typing import Any, Tuple
+import torchvision
+
+class NormalizeBPS(object):
+    def __call__(self, img_array) -> np.array(np.float32):
+        """
+        Normalize the array values between 0 - 1
+        """
+        return cv2.normalize(img_array, None, 0, 255, cv2.NORM_MINMAX).astype(np.float32)
+        #raise NotImplementedError
+class ResizeBPS(object):
+    def __init__(self, resize_height: int, resize_width:int):
+        self.resize_height = resize_height
+        self.resize_width = resize_width
+    
+    def __call__(self, img:np.ndarray) -> np.ndarray:
+        """
+        Resize the image to the specified width and height
+
+        args:
+            img (np.ndarray): image to be resized.
+        returns:
+            torch.Tensor: resized image.
+        """
+        return cv2.resize(img, (self.resize_width, self.resize_height), interpolation=cv2.INTER_AREA)
+        #raise NotImplementedError
+
+class VFlipBPS(object):
+    def __call__(self, image) -> np.ndarray:
+        """
+        Flip the image vertically
+        """
+        return cv2.flip(image, 0)
+        #raise NotImplementedError
+
+
+class HFlipBPS(object):
+    def __call__(self, image) -> np.ndarray:
+        """
+        Flip the image horizontally
+        """
+        return cv2.flip(image, 1)
+        #raise NotImplementedError
+
+
+class RotateBPS(object):
+    def __init__(self, rotate: int) -> None:
+        self.rotate = rotate
+
+    def __call__(self, image) -> Any:
+        '''
+        Initialize an object of the Augmentation class
+        Parameters:
+            rotate (int):
+                Optional parameter to specify a 90, 180, or 270 degrees of rotation.
+        Returns:
+            np.ndarray
+        '''
+        if self.rotate in [90, 180, 270]:
+            if self.rotate == 90:
+                angle = cv2.ROTATE_90_CLOCKWISE
+            elif self.rotate == 180:
+                angle = cv2.ROTATE_180
+            elif self.rotate == 270:
+                angle = cv2.ROTATE_90_COUNTERCLOCKWISE
+            image = cv2.rotate(image, angle)
+        return image
+        #raise NotImplementedError
+
+
+class RandomCropBPS(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+        is made.
+    """
+
+    def __init__(self, output_height: int, output_width: int):
+        self.output_height = output_height
+        self.output_width = output_width
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        top = np.random.randint(0, h - self.output_height)
+        left = np.random.randint(0, w - self.output_width)
+
+        bottom = top + self.output_height
+        right = left + self.output_width
+
+        return image[top:bottom, left:right]
+        #raise NotImplementedError
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, image: np.ndarray) -> torch.Tensor:
+        # numpy image: H x W x C
+        # torch image: C x H x W
+        if len(image.shape) == 2: 
+            image = np.expand_dims(image, axis=2)
+        image = image.transpose((2, 0, 1))  
+        return torch.from_numpy(image)
+       #raise NotImplementedError
+
+class ZoomBPS(object):
+    def __init__(self, zoom: float=1) -> None:
+        self.zoom = zoom
+
+    def __call__(self, image) -> np.ndarray:
+        s = image.shape
+        s1 = (int(self.zoom*s[0]), int(self.zoom*s[1]))
+        img = np.zeros((s[0], s[1]))
+        img_resize = cv2.resize(image, (s1[1],s1[0]), interpolation = cv2.INTER_AREA)
+        # Resize the image using zoom as scaling factor with area interpolation
+        if self.zoom < 1:
+            y1 = s[0]//2 - s1[0]//2
+            y2 = s[0]//2 + s1[0] - s1[0]//2
+            x1 = s[1]//2 - s1[1]//2
+            x2 = s[1]//2 + s1[1] - s1[1]//2
+            img[y1:y2, x1:x2] = img_resize
+            return img
+        else:
+            return img_resize
+
+def main():
+    """Driver function for testing the augmentations. Make sure the file paths work for you."""
+    # load image using cv2
+    img_key = 'P280_73668439105-F5_015_023_proj.tif'
+    img_array = cv2.imread(img_key, cv2.IMREAD_ANYDEPTH)
+    print(img_array.shape, img_array.dtype)
+    test_resize = ResizeBPS(500, 500)
+    type(test_resize)
+
+if __name__ == "__main__":
+    main()
---
+++ bps_dataset.py
@@ -0,0 +1,217 @@
+"""
+This module contains the BPSMouseDataset class which is a subclass of torch.utils.data.Dataset.
+"""
+from src.data_utils import get_bytesio_from_s3
+from src.dataset.augmentation import (
+    NormalizeBPS,
+    ResizeBPS,
+    VFlipBPS,
+    HFlipBPS,
+    RotateBPS,
+    RandomCropBPS,
+    ToTensor
+)
+
+import os
+import torch
+import cv2
+from torch.utils.data import Dataset, DataLoader
+
+import pytorch_lightning as pl
+from torchvision import transforms, utils
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+from matplotlib import pyplot as plt
+from pyprojroot import here
+
+root = here()
+
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+import io
+from io import BytesIO
+
+""" 
+Note about Tensor:
+        PyTorch expects tensors to have the following dimensions:
+        (batch_size, channels, height, width)
+        A numpy array has the following dimensions:
+        (height, width, channels)
+"""
+
+
+class BPSMouseDataset(torch.utils.data.Dataset):
+    """
+    Custom PyTorch Dataset class for the BPS microscopy data.
+
+    args:
+        meta_csv_file (str): name of the metadata csv file
+        meta_root_dir (str): path to the metadata csv file
+        bucket_name (str): name of bucket from AWS open source registry.
+        transform (callable, optional): Optional transform to be applied on a sample.
+
+    attributes:
+        meta_df (pd.DataFrame): dataframe containing the metadata
+        bucket_name (str): name of bucket from AWS open source registry.
+        train_df (pd.DataFrame): dataframe containing the metadata for the training set
+        test_df (pd.DataFrame): dataframe containing the metadata for the test set
+        transform (callable): The transform to be applied on a sample.
+
+    raises:
+        ValueError: if the metadata csv file does not exist
+    """
+
+    def __init__(
+            self,
+            meta_csv_file:str,
+            meta_root_dir:str,
+            s3_client: boto3.client = None,
+            bucket_name: str = None,
+            transform=None,
+            file_on_prem:bool = True):
+
+        # formulate the full path to metadata csv file
+        # if the file is not on the local file system, use the get_bytesio_from_s3 function
+        # to fetch the file as a BytesIO object, else read the file from the local file system.
+        #pass
+        full_path = os.path.join(meta_root_dir, meta_csv_file)
+        self.df = pd.DataFrame()
+        self.s3_file_path = 'Microscopy/train'
+        self.meta_csv_file = meta_csv_file
+        self.meta_root_dir = meta_root_dir
+        self.s3_client = s3_client
+        self.bucket_name = bucket_name
+        self.transform = transform
+        self.file_on_prem = file_on_prem
+
+        if os.path.isfile(full_path):
+            self.df = pd.read_csv(full_path)
+        else:
+            s3_path = os.path.join(self.s3_file_path, meta_csv_file)
+            bytes_io = get_bytesio_from_s3(s3_client, bucket_name, s3_path)
+            self.df = pd.read_csv(bytes_io)
+
+    def __len__(self):
+        """
+        Returns the number of images in the dataset.
+
+        returns:
+          len (int): number of images in the dataset
+        """
+        return len(self.df)
+        #raise NotImplementedError
+
+    def __getitem__(self, idx):
+        """
+        Fetches the image and corresponding label for a given index.
+
+        Args:
+            idx (int): index of the image to fetch
+
+        Returns:
+            img_tensor (torch.Tensor): tensor of image data
+            label (int): label of image
+        """
+
+        # get the bps image file name from the metadata dataframe at the given index
+        file_name = self.df.iloc[idx, 0]
+        # formulate path to image given the root directory (note meta.csv is in the
+        # same directory as the images)
+        file_path = os.path.join(self.meta_root_dir, file_name) 
+
+        # If on_prem is False, then fetch the image from s3 bucket using the get_bytesio_from_s3
+        # function, get the contents of the buffer returned, and convert it to a  numpy array
+        # with datatype unsigned 16 bit integer used to represent microscopy images.
+        # If on_prem is True load the image from local.             
+        if self.file_on_prem:
+            img = plt.imread(file_path)
+        else:
+            s3_path =  os.path.join(self.s3_file_path, file_name)
+            image_buffer = get_bytesio_from_s3(self.s3_client, self.bucket_name, s3_path)
+            img = np.frombuffer(image_buffer.getvalue(), dtype=np.uint16)
+        # apply tranformation if available
+        img = self.transform(img)
+
+        # return the image and associated label
+        return img, self.df.iloc[idx, 2]
+        #raise NotImplementedError
+
+
+def show_label_batch(image: torch.Tensor, label: str):
+    """Show image with label for a batch of samples."""
+    images_batch, label_batch = \
+            image, label
+    batch_size = len(images_batch)
+    im_size = images_batch.size(2)
+    grid_border_size = 2
+
+    # grid is a 4 dimensional tensor (channels, height, width, number of images/batch)
+    # images are 3 dimensional tensor (channels, height, width), where channels is 1
+    # utils.make_grid() takes a 4 dimensional tensor as input and returns a 3 dimensional tensor
+    # the returned tensor has the dimensions (channels, height, width), where channels is 3
+    # the returned tensor represents a grid of images
+    grid = utils.make_grid(images_batch)
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+    plt.title(f"Label: {label}")
+    plt.savefig('test_grid_1_batch.png')
+
+
+
+def main():
+    """main function to test PyTorch Dataset class (Make sure the directory structure points to where the data is stored)"""
+    bucket_name = "nasa-bps-training-data"
+    s3_path = "Microscopy/train"
+    s3_meta_csv_path = f"{s3_path}/meta.csv"
+
+    #### testing get file functions from s3 ####
+
+    local_file_path = "../data/raw"
+    local_train_csv_path = "../data/processed/meta_dose_hi_hr_4_post_exposure_train.csv"
+
+    print(root)
+
+
+    #### testing dataset class ####
+    train_csv_path = 'meta_dose_hi_hr_4_post_exposure_train.csv'
+    training_bps = BPSMouseDatasetLocal(train_csv_path, '../data/processed', transform=None, file_on_prem=True)
+    print(training_bps.__len__())
+    print(training_bps.__getitem__(0))
+
+    transformed_dataset = BPSMouseDataset(train_csv_path,
+                                           '../data/processed',
+                                           transform=transforms.Compose([
+                                               NormalizeBPS(),
+                                               ResizeBPS(224, 224),
+                                               VFlipBPS(),
+                                               HFlipBPS(),
+                                               RotateBPS(90),
+                                               RandomCropBPS(200, 200),
+                                               ToTensor()
+                                            ]),
+                                            file_on_prem=True
+                                           )
+
+    # Use Dataloader to package data for batching, shuffling, 
+    # and loading in parallel using multiprocessing workers
+    # Packaging is image, label
+    dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=2)
+
+    for batch, (image, label) in enumerate(dataloader):
+        print(batch, image, label)
+
+        if batch == 5:
+            show_label_batch(image, label)
+            print(image.shape)
+            break
+
+if __name__ == "__main__":
+    main()
+#The PyTorch Dataset class is an abstract class that is used to provide an interface for accessing all the samples
+# in your dataset. It inherits from the PyTorch torch.utils.data.Dataset class and overrides two methods:
+# __len__ and __getitem__. The __len__ method returns the number of samples in the dataset and the __getitem__
+
