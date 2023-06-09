1bf9023da14fad8cc4b127e39bb9b410a7e445c0
Darren Hoang <dahoang1@uci.edu>
2023-06-08 23:21:14 -0700

    Added script to split train/test data and save tifs to data/raw

--- tests/test_create_meta.py
+++ tests/test_create_meta.py
@@ -23,17 +23,38 @@ def main():
 
     print(meta_csv_path_fname)
 
+    meta_train_path_fname, meta_test_path_fname = train_test_split_subset_meta_dose_hr(
+        subset_meta_dose_hr_csv_path=meta_csv_path_fname,
+        test_size=0.2,
+        out_dir_csv=data_dir,
+        stratify_col="particle_type"
+    )
+
     """
     Saving tiffs from meta.csv for training entire dataset
     Uncomment when ready...
     """
+
+    ## Saves tiffs from meta_train.csv
+    print("Downloading tifs from meta_train.csv...")
     save_tiffs_local_from_s3(
         s3_client=s3_client,
         bucket_name=bucket_name,
         s3_path=s3_path,
-        local_fnames_meta_path=meta_csv_path_fname,
+        local_fnames_meta_path=meta_train_path_fname,
         save_file_path=data_dir
     )
+    
+    ## Saves tiffs from meta_test.csv
+    print("Downloading tifs from meta_test.csv...")
+    save_tiffs_local_from_s3(
+        s3_client=s3_client,
+        bucket_name=bucket_name,
+        s3_path=s3_path,
+        local_fnames_meta_path=meta_test_path_fname,
+        save_file_path=data_dir
+    )
+
     
 
 
