e2459ea0e238ec068c65d519e1307b5f9c5eb018
Darren Hoang <dahoang1@uci.edu>
2023-06-08 22:48:55 -0700

    Created test file to grab meta.csv to prepare for train on full dataset

---
+++ tests/test_create_meta.py
@@ -0,0 +1,41 @@
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+
+from src.data_utils import *
+import pyprojroot
+root = pyprojroot.find_root(pyprojroot.has_dir(".git"))
+
+
+def main():
+    data_dir = f'{root}/data/raw'
+    bucket_name = 'nasa-bps-training-data'
+    s3_path = 'Microscopy/train'
+    s3_meta_csv_path = f'{s3_path}/meta.csv'
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+
+    meta_csv_path_fname = get_file_from_s3(
+        s3_client=s3_client,
+        bucket_name=bucket_name,
+        s3_file_path=s3_meta_csv_path,
+        local_file_path=data_dir
+    )
+
+    print(meta_csv_path_fname)
+
+    """
+    Saving tiffs from meta.csv for training entire dataset
+    Uncomment when ready...
+    """
+    save_tiffs_local_from_s3(
+        s3_client=s3_client,
+        bucket_name=bucket_name,
+        s3_path=s3_path,
+        local_fnames_meta_path=meta_csv_path_fname,
+        save_file_path=data_dir
+    )
+    
+
+
+if __name__ == '__main__':
+    main()
